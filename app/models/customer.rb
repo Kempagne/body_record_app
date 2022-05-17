c65aed36491738056b1be0794322f79433473aac
Kempagne <kenta130712@gmail.com>
2022-05-17 09:43:17 +0900

    feat/IS_004_deviseの導入、既存scaffoldの削除

--- app/models/customer.rb
+++
@@ -1,4 +0,0 @@
-class Customer < ApplicationRecord
-  enum sex: { man: 0, woman: 1}
-  
-end
--- spec/factories/admin_users.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :admin_user do
-    name { "MyString" }
-    mail { "MyString" }
-  end
-end
